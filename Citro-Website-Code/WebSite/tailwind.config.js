d0dccef6406d2f87a9d716d45545503c40a65c6a
Boris Hernandez Jr <108431983+Boris713@users.noreply.github.com>
2023-07-15 17:40:52 -0500

    Add files via upload

---
+++ Citro-Website-Code/WebSite/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'top_bg':"url('/Users/rajpatel/Desktop/Hack/CitroHacks/WebSite/build/img/bg-image.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
+
